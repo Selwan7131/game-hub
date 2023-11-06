3413a8ce65126aae222294b4e9b4e87a33c1a301
Selwan <selwan.abdeltawab@gmail.com>
2023-11-06 19:47:43 +0100

    Icons and Score

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,7 @@
 import { Game } from "../hooks/useGames";
-import { Card, Heading, Image } from "@chakra-ui/react";
+import { Card, HStack, Heading, Image, Text } from "@chakra-ui/react";
+import PlatformIconList from "./PlatformIconList";
+import CriticScore from "./CriticScore";
 
 interface props {
   game: Game;
@@ -10,6 +12,12 @@ const GameCard = ({ game }: props) => {
     <Card borderRadius={10} overflow="hidden">
       <Image src={game.background_image} />
       <Heading fontSize="2xl">{game.name}</Heading>
+      <HStack justifyContent="space-between" padding={2}>
+        <PlatformIconList
+          platforms={game.parent_platforms.map((p) => p.platform)}
+        />
+        <CriticScore score={game.metacritic} />
+      </HStack>
     </Card>
   );
 };
