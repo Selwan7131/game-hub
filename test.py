3ee69019121e8fd99c59d6282bff0014a6629d75
Selwan <selwan.abdeltawab@gmail.com>
2023-11-12 13:10:15 +0100

    Implement Platform Selector

--- test.py
+++ test.py
@@ -1,25 +1,38 @@
-    
+class TrieNode:
+    def __init__(self):
+        self.children = {}
 
-def isPalindrome(grid1,grid2):
-    rows, cols = len(grid1), len(grid1[0])
-    visited = set()
-    def dfs(i,j):
-        if i not in range(rows) or j not in range(cols) or grid2[i][j] == 0 or (i,j) in visited:
-            return True
-        if grid2[i][j] != grid1[i][j]:
-            return False
-        visited.add((i,j))
-        return dfs(i+1, j) and dfs(i-1,j) and dfs(i,j+1) and dfs(i,j-1)
-    num = 0
-    for i in range(rows):
-        for j in range(cols):
-            if (i,j) in visited or grid2[i][j] == 0:
-                continue
-            if dfs(i,j):
-                num += 1
-    return num
-# print(isPalindrome([[1,0,1,0,1],[1,1,1,1,1],[0,0,0,0,0],[1,1,1,1,1],[1,0,1,0,1]],[[0,0,0,0,0],[1,1,1,1,1],[0,1,0,1,0],[0,1,0,1,0],[1,0,0,0,1]]))
-connections = [[1,0],[1,2],[3,2],[3,4]]
-for i, j in connections:
-    print(i, j)
+class WordDictionary:
 
+    def __init__(self):
+        self.node = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        cur = self.node
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = TrieNode()
+            cur = cur.children[c]
+
+
+    def search(self, word: str, curNode=None) -> bool:
+        cur = self.node
+        if curNode:
+            cur = curNode
+        for i in range(len(word)):
+            if word[i] == '.':
+                for child in cur.children.values():
+                    return self.search(word[i+1:], child)
+            elif word[i] not in cur.children:
+                return False
+            cur = cur.children[word[i]]
+        return True
+
+obj = WordDictionary()
+obj.addWord("a")
+obj.addWord("a")
+param_2 = obj.search(".")
+param_2 = obj.search("a")
+param_3 = obj.search("aa")
+param_4 = obj.search("a.")
+param_5 = obj.search(".a")
