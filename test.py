1c00316841d3499bbdf820c43eefb307344a65fa
Selwan <selwan.abdeltawab@gmail.com>
2023-11-12 22:56:30 +0100

    Sort games

--- test.py
+++ test.py
@@ -1,38 +1,22 @@
-class TrieNode:
-    def __init__(self):
-        self.children = {}
-
-class WordDictionary:
-
-    def __init__(self):
-        self.node = TrieNode()
-
-    def addWord(self, word: str) -> None:
-        cur = self.node
-        for c in word:
-            if c not in cur.children:
-                cur.children[c] = TrieNode()
-            cur = cur.children[c]
-
-
-    def search(self, word: str, curNode=None) -> bool:
-        cur = self.node
-        if curNode:
-            cur = curNode
-        for i in range(len(word)):
-            if word[i] == '.':
-                for child in cur.children.values():
-                    return self.search(word[i+1:], child)
-            elif word[i] not in cur.children:
-                return False
-            cur = cur.children[word[i]]
-        return True
-
-obj = WordDictionary()
-obj.addWord("a")
-obj.addWord("a")
-param_2 = obj.search(".")
-param_2 = obj.search("a")
-param_3 = obj.search("aa")
-param_4 = obj.search("a.")
-param_5 = obj.search(".a")
+def sortArray(nums):
+    if len(nums) == 1:
+        return nums
+    sorted1 = sortArray(nums[0: len(nums)//2])
+    sorted2 = sortArray(nums[len(nums)//2:])
+    res = []
+    i = j = 0
+    while i < len(sorted1) and j < len(sorted2):
+        if sorted1[i] <= sorted2[j]:
+            res.append(sorted1[i])
+            i += 1
+        else:
+            res.append(sorted2[j])
+            j += 1
+    if i < len(sorted1):
+        for k in range(i, len(sorted1)):
+            res.append(sorted1[i])
+    elif j < len(sorted2) :
+        for k in range(j, len(sorted2)):
+            res.append(sorted2[j])
+    return res
+print(sortArray([-1,2,-8,-10]))
